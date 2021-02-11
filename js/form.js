53dd14e503d567431e992c54b110fedbd20d9da7
Ludwin Granados <ludwingranados@gmail.com>
2021-02-10 20:37:52 -0800

    first commit

---
+++ js/form.js
@@ -0,0 +1,32 @@
+function validateEmailForm(){
+    var fname = document.getElementById('fname');
+    var lname = document.getElementById('lname');
+    var email = document.getElementById('email');
+    var errDiv = document.getElementById('errors');
+    var error = '';
+    var focusElem;
+
+    if(fname.value.length == 0){
+        error += "First Name is required<br />";
+        focusElem = fname;
+    }
+
+    if(lname.value.length == 0){
+        error += "Last Name is required<br />";
+        focusElem = lname;
+    }
+
+    if(email.value.length == 0){
+        error += "Email is required<br />";
+        if(focusElem == undefined){
+            focusElem = email;
+        }
+    }
+
+    if(error.length > 0){
+        errDiv.innerHTML = error;
+        focusElem.focus();
+        return false;
+    }
+    return true;
+}
